c3e8ff27f3d88502fe21e87d99f9b09dc4dca836
Michael Mangino <mmangino@elevatedrails.com>
2008-08-28 17:58:19 -0400

    Handle next parameter and more closely model restful_auth

--- generators/facebook_user/templates/controller.rb
+++ generators/facebook_user/templates/controller.rb
@@ -5,7 +5,6 @@ module FacebookerAuthentication
       includer.module_eval do
         helper_attr :current_user
         attr_accessor :current_user
-        before_filter :set_current_user
       end
     end
     
@@ -17,8 +16,22 @@ module FacebookerAuthentication
       if facebook_session and 
          facebook_session.secured? and 
          !request_is_facebook_tab?
-        self.current_user = <%=class_name%>.for_facebook_id(facebook_session.user.to_i,facebook_session) 
+         self.current_user = <%=class_name%>.for_facebook_id(facebook_session.user.to_i,facebook_session) 
       end
     end
+    
+    def facebook_login_required
+      @after_login_url = request.request_uri
+      debugger
+      if ensure_authenticated_to_facebook
+        set_current_user
+      end
+    end
+    
+    def after_login_url
+      @after_login_url.blank? ? nil : url_for(:controller=>request.request_uri,:only_path=>true)
+    end
+    
+    
   end
 end
